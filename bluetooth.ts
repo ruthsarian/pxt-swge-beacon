8d6180ac7948bea2f38c1460019bf685c61ea4fc
The Forgettable Mister Ruthsarian <ruthsarian@users.noreply.github.com>
2020-01-23 21:06:12 -0500

    We'll call this V2

--- bluetooth.ts
+++ bluetooth.ts
@@ -3,7 +3,7 @@ namespace bluetooth {
      * SWGE Beacon
      * @param zone [1-7] set the location id for the beacon
      */
-    //% blockId=swge_beacon block="Location Beacon for| Zone: %zone"
+    //% blockId=swge_beacon block="SWGE Beacon for| Zone: %zone"
     //% parts="bluetooth"
     //% shim=bluetooth::activateSwgeBeacon
     //% zone.min=1 zone.max=7 zone.defl=1
@@ -15,7 +15,7 @@ namespace bluetooth {
      * SWGE Location Beacon
      * @param zone [1-7] set the location id for the beacon
      */
-    //% blockId=swge_location_beacon block="Location Beacon for| Zone: %zone"
+    //% blockId=swge_location_beacon block="SWGE Location Beacon for| Zone: %zone"
     //% parts="bluetooth"
     //% shim=bluetooth::activateSwgeLocationBeacon
     //% zone.min=1 zone.max=7 zone.defl=1
@@ -28,7 +28,7 @@ namespace bluetooth {
      * @param manufacturerId [0-65535] the manufacturer's id
      * @param beaconData string ; the beacon data
      */
-    //% blockId=swge_generic_beacon block="Generic Beacon with| Manufacturer's ID: %manufacturerId|, Beacon Data: %beaconData"
+    //% blockId=swge_generic_beacon block="SWGE Generic Beacon with| Manufacturer's ID: %manufacturerId| Beacon Data: %beaconData"
     //% parts="bluetooth"
     //% shim=bluetooth::activateGenericBeacon
     //% manufacturerId.shadowOptions.toString=true
