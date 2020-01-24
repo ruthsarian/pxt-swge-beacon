0c08b3936b87e81877e63eda81c73e9f0db01ff9
The Forgettable Mister Ruthsarian <ruthsarian@users.noreply.github.com>
2020-01-23 21:01:06 -0500

    Cleaning up block descriptions

--- bluetooth.ts
+++ bluetooth.ts
@@ -1,38 +1,40 @@
 namespace bluetooth {
-
     /**
-     * Advertise SWGE Beacon
+     * SWGE Beacon
      * @param zone [1-7] set the location id for the beacon
      */
-    //% block
+    //% blockId=swge_beacon block="Location Beacon for| Zone: %zone"
     //% parts="bluetooth"
     //% shim=bluetooth::activateSwgeBeacon
     //% zone.min=1 zone.max=7 zone.defl=1
-    export function activateSwgeBeacon(zone: number) {
+    //% deprecated=true
+    export function activateSwgeBeacon(zone: number): void {
         return;
     }
-
     /**
-     * Advertise SWGE Location Beacon
+     * SWGE Location Beacon
      * @param zone [1-7] set the location id for the beacon
      */
-    //% block
+    //% blockId=swge_location_beacon block="Location Beacon for| Zone: %zone"
     //% parts="bluetooth"
     //% shim=bluetooth::activateSwgeLocationBeacon
     //% zone.min=1 zone.max=7 zone.defl=1
-    export function activateSwgeLocationBeacon(zone: number) {
+    //% inlineInputMode=external
+    export function activateSwgeLocationBeacon(zone: number): void {
         return;
     }
-
     /**
-     * Advertise Generic Beacon
+     * SWGE Generic Beacon
      * @param manufacturerId [0-65535] the manufacturer's id
      * @param beaconData string ; the beacon data
      */
-    //% block
+    //% blockId=swge_generic_beacon block="Generic Beacon with| Manufacturer's ID: %manufacturerId|, Beacon Data: %beaconData"
     //% parts="bluetooth"
     //% shim=bluetooth::activateGenericBeacon
-    export function activateGenericBeacon(manufacturerId: string, beaconData: string) {
+    //% manufacturerId.shadowOptions.toString=true
+    //% beaconData.shadowOptions.toString=true
+    //% inlineInputMode=external
+    export function activateGenericBeacon(manufacturerId: string, beaconData: string): void {
         return;
     }
 }
